f64e1cc45f4a991a05b2481977b6b22ffd92f1b8
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2023-03-13 14:35:43 +0100

    Ajout des images et les noms des projets

--- src/components/navigations/AllProjectNavigation.js
+++ src/components/navigations/AllProjectNavigation.js
@@ -6,45 +6,86 @@ const AllProjectNavigation = () => {
     <div className="all-project-container">
       <div className="project-right">
         <ul className="all-project-list">
-          <NavLink to="/project-01" className="all-project-items projet-01">
-            <li className="hover ">Projet 01</li>
+          <NavLink
+            to="https://www.reservia-project.arnaud-defrancq.fr/"
+            className="all-project-items projet-01"
+            target="_blank"
+          >
+            <li className="hover ">Projet Rerservia - OpenClassrooms</li>
             <div className="img-project-01 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-02" className="all-project-items projet-02">
-            <li className="hover">Projet 02</li>
+          <NavLink
+            to="https://www.ohmyfood-project.arnaud-defrancq.fr/"
+            className="all-project-items projet-02"
+            target="_blank"
+          >
+            <li className="hover">Projet OhMyFood! - OpenClassrooms</li>
             <div className="img-project-02 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-03" className="all-project-items projet-03">
-            <li className="hover">Projet 03</li>
+          <NavLink
+            to="https://www.bubble-project.arnaud-defrancq.fr/"
+            className="all-project-items projet-03"
+            target="_blank"
+          >
+            <li className="hover">Projet bubble shooter</li>
             <div className="img-project-03 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-04" className="all-project-items projet-04">
-            <li className="hover">Projet 04</li>
+          <NavLink
+            to="https://www.generateur-project.arnaud-defrancq.fr/"
+            className="all-project-items projet-04"
+            target="_blank"
+          >
+            <li className="hover">Projet Générateur mot de passe</li>
             <div className="img-project-04 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-05" className="all-project-items projet-05">
-            <li className="hover">Projet 05</li>
+          <NavLink
+            to="https://github.com/ArnaudDefrancq/P07_OpenClassrooms_16052022"
+            className="all-project-items projet-05"
+            target="_blank"
+          >
+            <li className="hover">Projet Groupomania - OpenClassrooms</li>
             <div className="img-project-05 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-06" className="all-project-items projet-06">
-            <li className="hover">Projet 06</li>
+          <NavLink
+            to="https://github.com/ArnaudDefrancq/P05_Openclassroom_07022022"
+            className="all-project-items projet-06"
+            target="_blank"
+          >
+            <li className="hover">Projet Kanap - OpenClassrooms</li>
             <div className="img-project-06 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-07" className="all-project-items projet-07">
-            <li className="hover">Projet 07</li>
+          <NavLink
+            to="https://quiz-pays.arnaud-defrancq.fr/"
+            className="all-project-items projet-07"
+            taget="_blank"
+          >
+            <li className="hover">Projet Quizz drapeaux</li>
             <div className="img-project-07 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-08" className="all-project-items projet-08">
-            <li className="hover">Projet 08</li>
+          <NavLink
+            to="https://projet-bonneteau.arnaud-defrancq.fr/"
+            className="all-project-items projet-08"
+            target="_blank"
+          >
+            <li className="hover">Projet Jeu de bonneteau</li>
             <div className="img-project-08 img-projet"></div>
           </NavLink>
-          <NavLink to="/project-09" className="all-project-items projet-09">
-            <li className="hover">Projet 09</li>
+          <NavLink
+            to="https://projet-memory.arnaud-defrancq.fr/"
+            className="all-project-items projet-09"
+            target="_blank"
+          >
+            <li className="hover">Projet Jeu de mémoire</li>
             <div className="img-project-09 img-projet"></div>
           </NavLink>
-          {/* <NavLink to="/project-10" className="all-project-items projet-10">
-            <li className="hover">Projet 10</li>
-          </NavLink> */}
+          <NavLink
+            to="https://snake.arnaud-defrancq.fr/"
+            className="all-project-items projet-10"
+            target="_blank"
+          >
+            <li className="hover">Projet Snake</li>
+            <div className="img-project-10 img-projet"></div>
+          </NavLink>
         </ul>
       </div>
     </div>
